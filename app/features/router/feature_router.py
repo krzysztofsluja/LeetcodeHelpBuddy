1208b261a697d95375d0fa796a57750e0e4b37b1
krzysztofsluja <slujakrzysztof6@gmail.com>
2025-08-24 22:16:45 +0200

    Update feature router to replace TestCaseGenerator with TestCaseGenerationService and add logging for request dispatching

--- app/features/router/feature_router.py
+++ app/features/router/feature_router.py
@@ -1,8 +1,7 @@
 from typing import Protocol, TypeVar
 
 from app.features.models import Feature, FeatureRequest, FeatureResponse
-from app.features.testcase.v1.generator.generator import TestCaseGenerator
-from pydantic import BaseModel
+from app.features.testcase.v1.service.testcase_service import TestCaseGenerationService
 
 Request = TypeVar('Request', bound=FeatureRequest)
 Response = TypeVar('Response', bound=FeatureResponse)
@@ -11,8 +10,9 @@ class FeatureService(Protocol[Request,Response]):
     async def handleRequest(self, request: Request) -> Response: ...
 
 _service_registry: dict[Feature, FeatureService] = {
-    Feature.TEST_CASE_GENERATION: TestCaseGenerator()
+    Feature.TEST_CASE_GENERATION: TestCaseGenerationService()
 }
 
 async def dispatch(feature: Feature, request: FeatureRequest) -> FeatureResponse:
+    print(f"Dispatching request to feature: {feature}")
     return await _service_registry[feature].handleRequest(request)
