253cf8dd1ee434a2b54f5019b40c8c6095b3981b
krzysztofsluja <slujakrzysztof6@gmail.com>
2025-09-15 23:28:41 +0200

    Refactor GetProblemDetailsPort interface to accept LeetCodeProblem object instead of converter and payload, improving clarity and usability.

--- app/domain/ports/api/leetcode.py
+++ app/domain/ports/api/leetcode.py
@@ -1,16 +1,15 @@
 from abc import ABC, abstractmethod
-from app.domain.shared.leetcode.models import LeetCodeProblemSlug, LeetCodeProblemDetails
+from app.domain.shared.leetcode.models import LeetCodeProblem, LeetCodeProblemSlug, LeetCodeProblemDetails
 from cattrs import Converter
 
 class GetProblemDetailsPort(ABC):
     @abstractmethod
-    def get_problem_details(self, converter: Converter, payload: dict) -> LeetCodeProblemDetails: 
+    def get_problem_details(self, problem: LeetCodeProblem) -> LeetCodeProblemDetails: 
         """
         Fetches problem details by its title slug.
 
         Args:
-            converter: The converter to use to convert the response to a LeetCodeProblemDetails object.
-            payload: The payload to use to get the problem details.
+            problem: The problem to get the details for.
 
         Returns:
             A LeetCodeProblemDetails object containing the details.
