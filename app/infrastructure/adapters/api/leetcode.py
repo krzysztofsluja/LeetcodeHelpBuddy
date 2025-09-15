6ced3336a5b5e3088f08c8a26c69aeeeb9ca6155
krzysztofsluja <slujakrzysztof6@gmail.com>
2025-09-15 23:26:42 +0200

    Implement AlfaLCGetProblemDetailsAdapter for LeetCode API, including environment variable loading and converter preparation for problem details retrieval

--- app/infrastructure/adapters/api/leetcode.py
+++ app/infrastructure/adapters/api/leetcode.py
@@ -1,3 +1,5 @@
+import os
+from sqlite3 import paramstyle
 from typing import Dict, Final, override
 from app.domain.ports.api.leetcode import GetProblemDetailsPort, QuestionSlugExtractorPort
 from app.domain.shared.exception.api.api_exception import LeetCodeApiError, LeetCodeProblemNotFoundError
@@ -5,22 +7,43 @@ from app.domain.shared.leetcode.models import LeetCodeProblem, LeetCodeProblemDe
 import requests
 import re
 from cattrs import Converter
+from dotenv import load_dotenv
 
+class AlfaLCGetProblemDetailsAdapter(GetProblemDetailsPort):
 
-class GetProblemDetailsAdapter(GetProblemDetailsPort):
-
-    def __init__(self, get_problem_details_endpoint: str):
-        self.get_problem_details_endpoint = get_problem_details_endpoint
+    def __init__(self):
+        # TODO: Implement this
+        load_dotenv()
+        if not os.getenv("ALFA_LEETCODE_API_URL"):
+            raise LeetCodeApiError("ALFA_LEETCODE_API_URL is not set")
+        api_url = os.getenv("ALFA_LEETCODE_API_URL")
+        self.get_problem_details_endpoint = api_url + "/select"
 
     @override
-    def get_problem_details(self, converter: Converter, payload: Dict[str, str]) -> LeetCodeProblemDetails:
+    def get_problem_details(self, problem: LeetCodeProblem) -> LeetCodeProblemDetails:
+        converter = self.__prepare_converter()
         try:
+            payload = {'titleSlug': problem.question_slug.question_slug}
             request = requests.get(self.get_problem_details_endpoint, params=payload)
             if request.status_code != 200:
                 raise LeetCodeApiError()
             return converter.structure(request.json(), LeetCodeProblemDetails)
         except Exception as e:
             raise LeetCodeApiError()
+
+    def __prepare_converter(self) -> Converter:
+        converter = Converter()
+        converter.register_structure_hook(
+            LeetCodeProblemDetails, 
+            lambda dct, _: LeetCodeProblemDetails(
+                question_slug=dct['titleSlug'],
+                question_title=dct['questionTitle'],
+                question_content=dct['question'],
+                example_testcases=dct['exampleTestcases'],
+                difficulty=dct['difficulty']
+            )
+        )
+        return converter
 
 class SimpleQuestionSlugExtractorAdapter(QuestionSlugExtractorPort):
 
